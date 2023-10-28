e17bff29fe9d0a5f0941dfb4fba66631f3f4603c
Armando Rosales <refill.1509@gmail.com>
2023-10-28 11:42:40 -0500

    Add class User in utils/Usuario.py

--- uacj_pay/views.py
+++ uacj_pay/views.py
@@ -1,12 +1,21 @@
+# Django modules
 from django.shortcuts import render
 from django.http import JsonResponse
 
+# AWS modules
+import boto3
+
+# My modules
+from utils.usuario import User
 
 """
     * Registro de usuario con aws cognito
     * Almacenar usuario en dynamoDB (lambda)
 """
 def crear_usuario(request):
+    # Definir usuario con cognito
+    # user = User()
+    # return JsonResponse(user.get_token())
     pass
 
 """
