60617e61f2f42424a6e828d07e7f6873793b685c
Armando Rosales <refill.1509@gmail.com>
2023-10-27 21:36:27 -0500

    Se agregaron algunos pasos a seguir en los casos de uso de crear usuario y token

--- uacj_pay/views.py
+++ uacj_pay/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+"""
+    * Registro de usuario con aws cognito
+    * Almacenar usuario en dynamoDB (lambda)
+"""
+def crear_usuario(request):
+    pass
+
+"""
+    * Verificar que el usuario exista
+    * Verificar que el token que genero anteriormente expiro
+    * Creacion de nuevo token
+    * Retornar token de acceso
+"""
+def generar_token(request):
+    return JsonResponse({"token": 'akjdhaskjdhas',
+                         "expire": ':v'})
+    
+    
