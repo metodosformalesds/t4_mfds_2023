fbc56a3559750253908ee0527716c18384aa7478
LeonardoPantoja <al214960@alumnos.uacj.mx>
2023-11-18 21:48:27 -0700

    Se agrego la funcion de procesar pagos
    Se agrego el metodo para pagos y verificacion de la cuenta en DynamoDB, falta verificar si la cuenta tiene saldo y conectarlo a DynamoDB

--- t4_mfds_2023/urls.py
+++ t4_mfds_2023/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from utils import Pagos
 import uacj_pay.views as uacj_pay_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', uacj_pay_views.generar_token)
+    path('token/', uacj_pay_views.generar_token),
+    path('payment/', Pagos.payment, name='payment'),
 ]
 
